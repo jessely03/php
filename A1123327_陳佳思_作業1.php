b719e2e86439864588210a310020559ca289ea08
jessely03 <a1123327@mail.nuk.edu.tw>
2026-03-27 02:51:49 +0800

    Add files via upload

---
+++ A1123327_陳佳思_作業1.php
@@ -0,0 +1,142 @@
+<HTML>
+    <HEAD>
+        <TITLE>
+            LETS'S GO 夏令營
+        </TITLE>
+    </HEAD>
+
+    <body bgcolor = #fffcc9>
+
+        <center>
+            <font color = #6B6325>
+            <h1><u>LET'S GO 夏令營 2026</u></h1>
+            <h2>Welcome to Summer Nature Camp 2026!</h2>
+            <img src="https://highlandsbiological.org/wp-content/uploads/2025/12/2026-summer-nature-camps.jpg"alt="Summer Camp" width="500">
+            </font>
+        </center>
+        <form action="" method="post">
+        <font size = "4">
+            <p>Get ready for an exciting summer full of learning, adventure, and unforgettable memories 🎉</p>
+            <p>In this camp, participants will enjoy a variety of activities, including team-building games, outdoor exploration, creative workshops, and themed learning sessions. Our goal is to help campers build confidence, develop social skills, and enhance problem-solving abilities—all while having fun!</p>
+        </font>
+        <h4>📅 Date: July 1, 2026 – July 10, 2026<br>
+        📍 Location: National University of Kaohsiung</h4>
+        <h3>📌 Before You Fill Out the Form:</h3>
+        <font size = "4">
+            <ol>
+            <li>Please complete all required fields to ensure your registration is processed smoothly</li>
+            <li>All information provided will be used only for camp communication and arrangements</li>
+            <li>After submission, we will contact you via email or phone to confirm your registration</li>
+            </ol>
+        </font>
+        
+        <center><h2>REGISTER NOW!!</h2><br></center>
+
+<table border="1" align="center" cellpadding="10" bgcolor="white">
+<tr>
+    <td colspan="2"><center><b>Personal Information</b></center></td>
+</tr>
+
+<tr>
+    <td colspan="2">
+        Name:
+        <input type="text" placeholder="your name" name="nName" required><br/>
+    </td>
+</tr>
+
+<tr>
+    <td>
+        Gender:<br>
+        男<input type="radio" name="mGender" value="male" required>
+        女<input type="radio" name="mGender" value="female"><br>
+
+    </td>
+
+    <td>
+        Birthday:<br>
+        <input type="date" name="birthday" required>
+    </td>
+</tr>
+
+<tr>
+    <td colspan="2">
+        Email:<br>
+        <input type="email" placeholder="XXX@gmail.com" name="email" required><br/>
+    </td>
+</tr>
+
+<tr>
+    <td colspan="2">
+        Phone:<br>
+        <input type="tel" placeholder="(+886)XXX-XXX-XXX" name="phone" required>
+    </td>
+</tr>
+
+<tr>
+    <td>
+        Where are you from:<br>
+        <select name="location" required>
+            <option value="" disabled selected>Choose one</option>
+            <option>Taipei</option>
+            <option>Hsinchu</option>
+            <option>Taichung</option>
+            <option>Tainan</option>
+            <option>Kaohsiung</option>
+            <option>Pingtung</option>
+        </select>
+    </td>
+
+    <td>
+        Transportation:<br>
+        <select name="transport" required>
+            <option value="" disabled selected>Choose one</option>
+            <option>Bus</option>
+            <option>Private</option>
+        </select>
+    </td>
+</tr>
+
+<tr>
+    <td colspan="2">
+        Visit location:<br>
+        <input type="checkbox"> Taipei
+        <input type="checkbox"> Taichung
+        <input type="checkbox"> Kaohsiung
+    </td>
+</tr>
+
+<tr>
+    <td colspan="2">
+        Interest:<br>
+        <input type="checkbox"> Games
+        <input type="checkbox"> Sports
+        <input type="checkbox"> Movie
+        <input type="checkbox"> Music
+        <input type="checkbox"> Others:
+        <input type="text" name="interest_other"> 
+    </td>
+</tr>
+
+<tr>
+    <td colspan="2">
+        Message:<br>
+        <textarea rows="4" cols="68"></textarea>
+    </td>
+</tr>
+
+<tr>
+    <td align="center">
+        <input type="reset" value="Clear">
+    </td>
+    <td align="center">
+        <input type="submit" value="Send">
+    </td>
+    
+</tr>
+
+</table>
+
+</form>
+    </body>
+
+</HTML>
